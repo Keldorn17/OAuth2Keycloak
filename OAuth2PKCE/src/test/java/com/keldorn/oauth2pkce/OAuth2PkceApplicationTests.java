57b59ceae7f8e98d92854dba2ac6ff995496d500
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-11-28 14:17:57 +0100

    PCKE

---
+++ OAuth2PKCE/src/test/java/com/keldorn/oauth2pkce/OAuth2PkceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keldorn.oauth2pkce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2PkceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
